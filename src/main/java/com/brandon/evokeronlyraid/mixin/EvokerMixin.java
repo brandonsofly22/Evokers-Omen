730036f29c0a2bf7e7b81e2a2878916ebb79ffe5
brandonsofly22 <brandonsolfly22@outlook.com>
2026-09-22 21:10:18 -0500

    Sync exact potion and Evoker mixin sources

--- src/main/java/com/brandon/evokeronlyraid/mixin/EvokerMixin.java
+++ src/main/java/com/brandon/evokeronlyraid/mixin/EvokerMixin.java
@@ -1,11 +1,23 @@
 package com.brandon.evokeronlyraid.mixin;
+
 import com.brandon.evokeronlyraid.access.EvokerRaidMobData;
 import net.minecraft.world.entity.monster.illager.Evoker;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
+
 @Mixin(Evoker.class)
 public abstract class EvokerMixin implements EvokerRaidMobData {
- @Unique private int evokerOnlyRaid$omenLevel = 0;
- @Override public int evokerOnlyRaid$getOmenLevel(){return evokerOnlyRaid$omenLevel;}
- @Override public void evokerOnlyRaid$setOmenLevel(int level){this.evokerOnlyRaid$omenLevel=level;}
+
+    @Unique
+    private int evokerOnlyRaid$omenLevel = 0;
+
+    @Override
+    public int evokerOnlyRaid$getOmenLevel() {
+        return evokerOnlyRaid$omenLevel;
+    }
+
+    @Override
+    public void evokerOnlyRaid$setOmenLevel(int level) {
+        this.evokerOnlyRaid$omenLevel = level;
+    }
 }
