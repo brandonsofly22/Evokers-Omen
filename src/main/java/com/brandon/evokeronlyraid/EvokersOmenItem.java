e4bf7631905b7e6cee8c19e4119d5f272194330f
brandonsofly22 <brandonsolfly22@outlook.com>
2026-09-22 20:18:41 -0500

    Add Evoker's Omen source files

---
+++ src/main/java/com/brandon/evokeronlyraid/EvokersOmenItem.java
@@ -0,0 +1,10 @@
+package com.brandon.evokeronlyraid;
+
+import net.minecraft.world.item.Item;
+
+public class EvokersOmenItem extends Item {
+
+    public EvokersOmenItem(Properties properties) {
+        super(properties);
+    }
+}
